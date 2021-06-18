d60b4cb562402db859293694db40ff8ce00f9759
Marty Stumpf <thealmartyblog@gmail.com>
2021-06-18 11:57:23 -0700

    WIP! Fns mostly in place. Working on a type class for u/plc.

--- plutus-core/executables/uplc/Main.hs
+++ plutus-core/executables/uplc/Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE LambdaCase       #-}
 {-# LANGUAGE TypeApplications #-}
 
-module UPLC.Main (main, Program) where
+module Main (main) where
 
 import           PlutusPrelude                            (through)
 
@@ -52,14 +52,6 @@ import           System.Mem                               (performGC)
 import           Text.Printf                              (printf)
 import           Text.Read                                (readMaybe)
 
-type Program a = UPLC.Program PLC.Name PLC.DefaultUni PLC.DefaultFun a
-
--- UPLC supports name or de Bruijn indices when (de)serialising ASTs
-data AstNameType =
-  Named
-  | DeBruijn
-  deriving (Show)
-
 -- | Untyped AST with names consisting solely of De Bruijn indices. This is
 -- currently only used for intermediate values during CBOR/Flat
 -- serialisation/deserialisation.  We may wish to add TypedProgramDeBruijn as
@@ -67,8 +59,23 @@ data AstNameType =
 -- support for this is lacking elsewhere at the moment.
 type UntypedProgramDeBruijn a = UPLC.Program UPLC.DeBruijn PLC.DefaultUni PLC.DefaultFun a
 
-uplchelpText :: String
-uplchelpText = helpText "Untyped Plutus Core"
+uplcHelpText :: String
+uplcHelpText = helpText "Untyped Plutus Core"
+
+uplcInfoCommand :: ParserInfo Command
+uplcInfoCommand = plutus "Untyped Plutus Core Tool" uplcHelpText
+
+---------------- Name conversions ----------------
+
+-- | Convert an untyped de-Bruijn-indexed program to one with standard names.
+-- We have nothing to base the names on, so every variable is named "v" (but
+-- with a Unique for disambiguation).  Again, we don't support typed programs.
+fromDeBruijn :: UntypedProgramDeBruijn a -> IO (UntypedProgram a)
+fromDeBruijn prog = do
+    let namedProgram = UPLC.programMapNames (\(UPLC.DeBruijn ix) -> UPLC.NamedDeBruijn "v" ix) prog
+    case PLC.runQuote $ runExceptT @UPLC.FreeVariableError $ UPLC.unDeBruijnProgram namedProgram of
+      Left e  -> errorWithoutStackTrace $ show e
+      Right p -> return p
 
 -- | Convert an untyped program to one where the 'name' type is de Bruijn indices.
 toDeBruijn :: UntypedProgram a -> IO (UntypedProgramDeBruijn a)
@@ -84,16 +91,6 @@ serialiseDbProgramCBOR (UntypedProgram p) = UPLC.serialiseOmittingUnits <$> toDe
 -- | Convert names to de Bruijn indices and then serialise
 serialiseDbProgramFlat :: Flat a => Program a -> IO BSL.ByteString
 serialiseDbProgramFlat (UntypedProgram p) = BSL.fromStrict . flat <$> toDeBruijn p
-
--- | Convert an untyped de-Bruijn-indexed program to one with standard names.
--- We have nothing to base the names on, so every variable is named "v" (but
--- with a Unique for disambiguation).  Again, we don't support typed programs.
-fromDeBruijn :: UntypedProgramDeBruijn a -> IO (UntypedProgram a)
-fromDeBruijn prog = do
-    let namedProgram = UPLC.programMapNames (\(UPLC.DeBruijn ix) -> UPLC.NamedDeBruijn "v" ix) prog
-    case PLC.runQuote $ runExceptT @UPLC.FreeVariableError $ UPLC.unDeBruijnProgram namedProgram of
-      Left e  -> errorWithoutStackTrace $ show e
-      Right p -> return p
 
 parseUplcInput = parseInput UPLC.parseProgram checkProgram
 
@@ -122,6 +119,13 @@ loadASTfromFlat flatMode inp =
                Left e  -> errorWithoutStackTrace $ "Flat deserialisation failure: " ++ show e
                Right r -> return $ wrapper r
 
+---------------- Reading programs from files ----------------
+
+parseUplcInput :: Input
+  -> IO
+     (UPLC.Program
+        PLC.Name PLC.DefaultUni PLC.DefaultFun PLC.AlexPosn)
+parseUplcInput = parseInput UPLC.parseProgram checkProgram
 
 -- Read either a PLC file or a CBOR file, depending on 'fmt'
 getProgram :: Format -> Input  -> IO (Program PLC.AlexPosn)
@@ -196,6 +200,86 @@ runEval (EvalOptions inp ifmt evalMode printMode budgetMode timingMode cekModel)
 
 runUplcPrint :: PrintOptions -> IO ()
 runUplcPrint = runPrint parseUplcInput
+
+
+---------------- Printing budgets and costs ----------------
+
+printBudgetStateBudget :: UPLC.Term UPLC.Name PLC.DefaultUni PLC.DefaultFun () -> CekModel -> ExBudget -> IO ()
+printBudgetStateBudget _ model b =
+    case model of
+      Unit -> pure ()
+      _ ->  let ExCPU cpu = _exBudgetCPU b
+                ExMemory mem = _exBudgetMemory b
+            in do
+              putStrLn $ "CPU budget:    " ++ show cpu
+              putStrLn $ "Memory budget: " ++ show mem
+
+printBudgetStateTally :: (Eq fun, Cek.Hashable fun, Show fun)
+       => UPLC.Term UPLC.Name PLC.DefaultUni PLC.DefaultFun () -> CekModel ->  Cek.CekExTally fun -> IO ()
+printBudgetStateTally term model (Cek.CekExTally costs) = do
+  putStrLn $ "Const      " ++ pbudget (Cek.BStep Cek.BConst)
+  putStrLn $ "Var        " ++ pbudget (Cek.BStep Cek.BVar)
+  putStrLn $ "LamAbs     " ++ pbudget (Cek.BStep Cek.BLamAbs)
+  putStrLn $ "Apply      " ++ pbudget (Cek.BStep Cek.BApply)
+  putStrLn $ "Delay      " ++ pbudget (Cek.BStep Cek.BDelay)
+  putStrLn $ "Force      " ++ pbudget (Cek.BStep Cek.BForce)
+  putStrLn $ "Builtin    " ++ pbudget (Cek.BStep Cek.BBuiltin)
+  putStrLn ""
+  putStrLn $ "startup    " ++ pbudget Cek.BStartup
+  putStrLn $ "compute    " ++ printf "%-20s" (budgetToString totalComputeCost)
+  putStrLn $ "AST nodes  " ++ printf "%15d" (UPLC.termSize term)
+  putStrLn ""
+  putStrLn $ "BuiltinApp " ++ budgetToString builtinCosts
+  case model of
+    Default ->
+        do
+  -- 1e9*(0.200  + 0.0000725 * totalComputeSteps + builtinExeTimes/1000)  putStrLn ""
+          putStrLn ""
+          traverse_ (\(b,cost) -> putStrLn $ printf "%-20s %s" (show b) (budgetToString cost :: String)) builtinsAndCosts
+          putStrLn ""
+          putStrLn $ "Total budget spent: " ++ printf (budgetToString totalCost)
+          putStrLn $ "Predicted execution time: " ++ formatTimePicoseconds totalTime
+    Unit -> pure ()
+  where
+        getSpent k =
+            case H.lookup k costs of
+              Just v  -> v
+              Nothing -> ExBudget 0 0
+        allNodeTags = fmap Cek.BStep [Cek.BConst, Cek.BVar, Cek.BLamAbs, Cek.BApply, Cek.BDelay, Cek.BForce, Cek.BBuiltin]
+        totalComputeCost = mconcat $ map getSpent allNodeTags  -- For unitCekCosts this will be the total number of compute steps
+        budgetToString (ExBudget (ExCPU cpu) (ExMemory mem)) =
+            printf "%15s  %15s" (show cpu) (show mem) :: String -- Not %d: doesn't work when CostingInteger is SatInt.
+        pbudget = budgetToString . getSpent
+        f l e = case e of {(Cek.BBuiltinApp b, cost)  -> (b,cost):l; _ -> l}
+        builtinsAndCosts = List.foldl f [] (H.toList costs)
+        builtinCosts = mconcat (map snd builtinsAndCosts)
+        -- ^ Total builtin evaluation time (according to the models) in picoseconds (units depend on BuiltinCostModel.costMultiplier)
+        getCPU b = let ExCPU b' = _exBudgetCPU b in fromIntegral b'::Double
+        totalCost = getSpent Cek.BStartup <> totalComputeCost <> builtinCosts
+        totalTime = getCPU (getSpent Cek.BStartup) + getCPU totalComputeCost + getCPU builtinCosts
+
+class PrintBudgetState cost where
+    printBudgetState :: UPLC.Term PLC.Name PLC.DefaultUni PLC.DefaultFun () -> CekModel -> cost -> IO ()
+    -- TODO: Tidy this up.  We're passing in the term and the CEK cost model
+    -- here, but we only need them in tallying mode (where we need the term so
+    -- we can print out the AST size and we need the model type to decide how
+    -- much information we're going to print out).
+
+instance PrintBudgetState Cek.CountingSt where
+    printBudgetState term model (Cek.CountingSt budget) = printBudgetStateBudget term model budget
+
+instance (Eq fun, Cek.Hashable fun, Show fun) => PrintBudgetState (Cek.TallyingSt fun) where
+    printBudgetState term model (Cek.TallyingSt tally budget) = do
+        printBudgetStateBudget term model budget
+        putStrLn ""
+        printBudgetStateTally term model tally
+
+instance PrintBudgetState Cek.RestrictingSt where
+    printBudgetState term model (Cek.RestrictingSt (ExRestrictingBudget budget)) =
+        printBudgetStateBudget term model budget
+
+
+
 
 main :: IO ()
 main = do
